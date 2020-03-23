d61379a5bdc92735f56a1391b2901fd41dbffa49
Elbo Shindi Pangestu <elbo@engineer.com>
2020-03-23 08:56:18 +0700

    add Vue Datatables

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Hospital;
 use Illuminate\Http\Request;
 
 class HospitalController extends Controller
 {
-    //
+    public function index()
+    {
+        $hospitals = Hospital::orderBy(request()->sortby ?? 'id', request()->sortbydesc ?? 'asc')
+                        ->when(request()->q, function($hospitals) {
+                            $hospitals = $hospitals->where('name', 'LIKE', '%' . request()->q . '%')
+                                                   ->orWhere('phone', 'LIKE', '%' . request()->q . '%')
+                                                   ->orWhere('address', 'LIKE', '%' . request()->q . '%');
+                        })->paginate(request()->per_page ?? 10);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $hospitals
+        ], 200);
+    }
 }
